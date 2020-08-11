8b5c08f7474bff4b9b0bbd74aa7972e861e0e58f
Annasarp-blr <pras_sunny@yahoo.com>
2020-08-11 07:33:25 +0530

    First Commit

---
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController  {
+
+    public String getMessage() {
+        return "My message";
+
+    }
+}
