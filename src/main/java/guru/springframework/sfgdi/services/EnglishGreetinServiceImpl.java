ef6f927376d41dd914d8d703248775e37e4171bf
Annasarp-blr <pras_sunny@yahoo.com>
2020-08-11 13:11:05 +0530

    First Commit

--- src/main/java/guru/springframework/sfgdi/services/EnglishGreetinServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/EnglishGreetinServiceImpl.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("GreetingService")
-@Profile(value="ES")
-public class GreetingServiceImpl implements GreetingService {
+@Profile(value="EN")
+public class EnglishGreetinServiceImpl implements  GreetingService {
 
     @Override
     public String getGreeting() {
-        return "Hello World!!!";
+        return  "Hello World - Profile EN";
     }
 }
