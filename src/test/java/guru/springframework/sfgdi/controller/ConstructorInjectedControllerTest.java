5c62005e69775a6bba576b40949f9d83793157bf
Annasarp-blr <pras_sunny@yahoo.com>
2020-08-11 09:28:30 +0530

    First Commit

---
+++ src/test/java/guru/springframework/sfgdi/controller/ConstructorInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controller;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public  void getGreeting() {
+     controller.getGreeting();
+
+    }
+
+}
