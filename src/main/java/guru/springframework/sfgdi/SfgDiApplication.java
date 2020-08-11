e40fd24aec875f5ab1b60516e4ef5e6d6b9e047e
Annasarp-blr <pras_sunny@yahoo.com>
2020-08-11 07:39:38 +0530

    First Commit

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,17 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+	  ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+		System.out.println(((MyController) context.getBean("myController")).getMessage() );
 	}
 
 }
